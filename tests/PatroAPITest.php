35ec7bb210836b01d4a227d380d9c6d8072e888e
Bibek Lekhak <bibek@computingcastle.com>
2021-06-06 15:29:08 +0545

    Test date conversion bs to ad and vice-versa

--- tests/PatroAPITest.php
+++ tests/PatroAPITest.php
@@ -16,6 +16,13 @@ class PatroAPITest extends TestCase
         $this->Cal = new \PatroAPI\NepaliCalendar();
     }
 
+
+    public function testTwoDateConversions(): void
+    {
+        $this->assertSame($this->Cal->eng_to_nep(2008,11,23), ['year' => 2065, 'month' => 8, 'date' => 8, 'day' => 'Sunday', 'nmonth' => 'Mangshir', 'num_day' => 1]);
+        $this->assertSame($this->Cal->nep_to_eng(2065,8,8), ['year' => 2008, 'month' => 11, 'date' => 23, 'day' => 'Sunday', 'emonth' => '', 'num_day' => 1, 'nmonth' => 'November']);
+    }
+
     /**
      * @covers NepaliCalendar::_get_day_of_week
      */
