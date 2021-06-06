4eb954a0c21bac927fd832545ed6b70f4c055a48
Bibek Lekhak <bibek@computingcastle.com>
2021-06-06 15:36:22 +0545

    Reduce horizontal spread for readability

--- tests/PatroAPITest.php
+++ tests/PatroAPITest.php
@@ -19,8 +19,14 @@ class PatroAPITest extends TestCase
 
     public function testTwoDateConversions(): void
     {
-        $this->assertSame($this->Cal->eng_to_nep(2008,11,23), ['year' => 2065, 'month' => 8, 'date' => 8, 'day' => 'Sunday', 'nmonth' => 'Mangshir', 'num_day' => 1]);
-        $this->assertSame($this->Cal->nep_to_eng(2065,8,8), ['year' => 2008, 'month' => 11, 'date' => 23, 'day' => 'Sunday', 'emonth' => 'November', 'num_day' => 1]);
+        $this->assertSame(
+            $this->Cal->eng_to_nep(2008,11,23), 
+            ['year' => 2065, 'month' => 8, 'date' => 8, 'day' => 'Sunday', 'nmonth' => 'Mangshir', 'num_day' => 1]
+        );
+        $this->assertSame(
+            $this->Cal->nep_to_eng(2065,8,8), 
+            ['year' => 2008, 'month' => 11, 'date' => 23, 'day' => 'Sunday', 'emonth' => 'November', 'num_day' => 1]
+        );
     }
 
     protected function tearDown(): void
